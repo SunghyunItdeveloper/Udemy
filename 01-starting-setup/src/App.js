c7a55076653fff2d523d3dd9dc6896f919d47ebc
leoa4238 <121601423+leoa4238@users.noreply.github.com>
2023-07-11 15:14:43 +0900

    folder

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,5 +1,4 @@
-import Expenses from "./components/Expenses";
-
+import Expenses from "./components/Expenses/Expenses";
 function App() {
   const expenses = [
     {
@@ -22,6 +21,13 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's getStarted!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
     <div>
